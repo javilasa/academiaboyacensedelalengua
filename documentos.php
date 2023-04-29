cf5ef40ff1bee0d9d367c80132bd362a3e5d2d19
Javier Avila <ja_avila@hotmail.com>
2023-04-29 10:15:20 -0500

    modificando menú de documentos

--- documentos.php
+++ documentos.php
@@ -31,8 +31,8 @@
             <div class="col-11 col-sm-10 col-md-8">
                 <h2 class="text-dark">Documentos</h2>
                 <ul class="list-group">
-                  <li class="list-group-item"><i class="far fa-file"></i><a href="/assets/files/docs/ESTADOS_FINANCIEROS_ACADEMIA_2021.pdf">  Estados Financieros 2021</a></li>
-                  <li class="list-group-item"><i class="far fa-file"></i><a href="/assets/files/docs/INFORME_DE_GESTION_ACADEMIA_2021.pdf">  Informe Gestión 2021</a></li>
+                  <li class="list-group-item"><i class="far fa-file"></i><a href="/assets/files/docs/ESTADOS_FINANCIEROS_ACADEMIA_2021.pdf">  Estados Financieros 2022</a></li>
+                  <li class="list-group-item"><i class="far fa-file"></i><a href="/assets/files/docs/INFORME_DE_GESTION_ACADEMIA_2021.pdf">  Informe Gestión 2022</a></li>
                   <li class="list-group-item"><i class="far fa-file"></i><a href="/assets/files/docs/CERTIFICADO_DE_ANTECEDENTES_MIEMBROS_DIRECTIVOS_Y_FUNADADORES_ACADEMIA_2021.pdf">  Certificado Antecedentes Miembros Directivos</a></li>
                   <li class="list-group-item"><i class="far fa-file"></i><a href="/assets/files/docs/CERTIFICADO_DE_CUMPLIMIENTO_DE_REQUISITOS_2021.pdf">  Certificado de Cumplimiento de Requisitos 2021</a></li>
                   <li class="list-group-item"><i class="far fa-file"></i><a href="/assets/files/docs/1116602405352-PRESENTACION_RENTA_AÑO_2020_ACADEMIA.pdf">  Presentación Renta - Academia 2020</a></a></li>
